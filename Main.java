4635a2db8a38d54154ac20b766af80dbd0cbaa4b
Ryan Bie <30966505+ryanbie@users.noreply.github.com>
2021-10-25 15:37:33 -0400

    Update Main.java
    Added an action listener to count the number of clicks on the button.

--- Main.java
+++ Main.java
@@ -1,22 +1,30 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main {
+public class Main implements ActionListener {
+
+    private int count = 0;
+    private JLabel label;
+    private JFrame frame;
+    private JPanel panel;
 
     public Main() { //constructor
 
-        JFrame frame = new JFrame();
-        
+        frame = new JFrame();
+
         JButton button = new JButton("Click Me");
-        
-        JLabel label = new JLabel("Number of clicks: ");
-        
-        JPanel panel = new JPanel();
+
+        button.addActionListener(this);
+
+        label = new JLabel();
+
+        panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
         panel.add(button);
         panel.add(label);
-        
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -24,11 +32,16 @@ public class Main {
         frame.pack();
         frame.setVisible(true);
 
-
     }
 
     public static void main(String[] args) {
         new Main();
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Number of clicks: " + count);
     }
 }
