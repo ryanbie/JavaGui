5a3d4d5694ebc902fb3afd6800212fb871d18242
Ryan Bie <30966505+ryanbie@users.noreply.github.com>
2021-10-25 15:08:20 -0400

    Update Main.java
    Added a button

--- Main.java
+++ Main.java
@@ -6,9 +6,14 @@ public class Main {
     public Main() { //constructor
 
         JFrame frame = new JFrame();
+        
+        JButton button = new JButton("Click Me");
+        
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
